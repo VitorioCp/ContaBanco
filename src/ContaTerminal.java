b07f17ba5b740469e70af70b7b2b2dd182d2f3a3
Vitório Corrêa <vitcorreapereira@hotmail.com>
2024-09-25 23:16:01 -0300

    fix lógica POO

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,41 +1,61 @@
-
 import java.util.Scanner;
 
+class Conta {
+    private String nomeCliente;
+    private String agencia;
+    private String conta;
+    private String saldo;
+
+    public Conta(String nomeCliente, String agencia, String conta, String saldo) {
+        this.nomeCliente = nomeCliente;
+        this.agencia = agencia;
+        this.conta = conta;
+        this.saldo = saldo;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public String getConta() {
+        return conta;
+    }
+
+    public String getSaldo() {
+        return saldo;
+    }
+}
+
 public class ContaTerminal {
-
-    private static final Scanner scanner = new Scanner(System.in);
-
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String nomeCliente = NomeCliente();
-        String agencia = Agencia();
-        String conta = Conta();
-        String saldo = saldo();
-
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+        Conta conta = criarConta();
+        exibirMensagem(conta);
     }
 
-    public static String NomeCliente() {
-        System.out.print("Digite seu nome: ");
-        return scanner.nextLine();  
+    private static Conta criarConta() {
+        String nomeCliente = obterEntrada("Digite seu nome: ");
+        String agencia = obterEntrada("Digite sua agência: ");
+        String contaNumero = obterEntrada("Digite sua conta: ");
+        String saldo = obterEntrada("Digite seu saldo: ");
+
+        return new Conta(nomeCliente, agencia, contaNumero, saldo);
     }
 
-    public static String Agencia() {
-        System.out.print("Digite sua agencia: ");
-        return scanner.nextLine();  
+    private static String obterEntrada(String mensagem) {
+        System.out.print(mensagem);
+        return scanner.nextLine();
     }
 
-    public static String Conta() {
-        System.out.print("Digite sua conta: ");
-        return scanner.nextLine();  
-    }
-
-    public static String saldo() {
-        System.out.print("Digite seu saldo: ");
-        return scanner.nextLine(); 
-    }
-
-    public static Scanner getScanner() {
-        return scanner;
+    private static void exibirMensagem(Conta conta) {
+        System.out.println("\nOlá " + conta.getNomeCliente() +
+                "obrigado por criar uma conta em nosso banco, sua agência é " + conta.getAgencia() +
+                ", sua conta é " + conta.getConta() +
+                " e seu saldo é " + conta.getSaldo() + " já está disponível para saque.");
     }
 }
